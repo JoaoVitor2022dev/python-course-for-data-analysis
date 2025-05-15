e37eb3ffb6aefa4d27ec07b704297a00a66152e5
Joao Vitor <githubconta2021@gmail.com>
2025-05-15 11:56:46 -0300

    Description: Importações de biblioteca em python

--- 3. Bibliotecas/biblioteca.py
+++ 3. Bibliotecas/biblioteca.py
@@ -18,8 +18,35 @@ print(f'{raiz2:.3f}')
 # Renomeando o math para sqrt para poder refatorar o código
 from math import sqrt
 
-raiz3 = sqrt(64)
+raiz3 = sqrt(16)
 print(f'{raiz3:.2}')
+
+# impotantando funcção matematica, valor fatorial 
+
+from math import sqrt, factorial
+
+fat = factorial(4)
+
+print(f'valor fatorial {fat}')     # valor fatorial 5! 5.4.3.2 = 120
+
+
+# O fatorial não pode fazer o calculo com numero fracionado 
+
+faturial_raiz = factorial(int(sqrt(16)))
+
+faturial_raiz
+
+# Números aleatório com a radon
+
+from random import random, randint 
+
+num = randint(1,10)
+
+print(num)
+
+radonnum = random()
+
+print(radonnum)
 
 
 
